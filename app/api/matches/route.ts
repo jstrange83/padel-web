b4ace061fbd751d5862204ccc43b45bd0eb3a72b
jstrange83 <jstrange83@gmail.com>
2025-09-20 15:12:45 +0200

    Update route.ts

--- app/api/matches/route.ts
+++ app/api/matches/route.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { NextResponse } from 'next/server';
 import { prisma } from '@/prisma/client';
 import { calcElo } from '@/lib/elo';
