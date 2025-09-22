ce350fb68c0a22b02a7e3b4c5f770c12a6d41627
jstrange83 <jstrange83@gmail.com>
2025-09-22 12:28:38 +0200

    Update page.tsx

--- app/matches/new/page.tsx
+++ app/matches/new/page.tsx
@@ -5,13 +5,11 @@ import NewMatchForm from "./ui/NewMatchForm";
 const prisma = new PrismaClient();
 
 export default async function Page() {
-  // Hent aktive spillere direkte fra DB på serveren
   const players = await prisma.user.findMany({
     where: { isActive: true },
     orderBy: { name: "asc" },
     select: { id: true, name: true },
   });
 
-  // Giv dem til klient-komponenten som props
   return <NewMatchForm players={players} />;
 }
